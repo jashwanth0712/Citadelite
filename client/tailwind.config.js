da673ca7b37ef6dd690c25c3f089db226802662f
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-03 15:47:17 +0530

    changed the name

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        mf: "990px",
+      }
+    },
+  },
+  plugins: [],
+}
