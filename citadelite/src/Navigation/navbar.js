edf671b48f32cf287e0f887e180a3a25ec662bb5
JustinBenito <justinbenito1974@gmail.com>
2022-12-03 10:10:44 +0530

    navbar

--- citadelite/src/Navigation/navbar.js
+++ citadelite/src/Navigation/navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav class="navbar navbar-expand-lg navbar-light bg-dark justify-content-between">
   <a class="navbar-brand text-white" href="#">Citadelite</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon white"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
